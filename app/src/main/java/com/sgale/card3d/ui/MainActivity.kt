9777bdd29cddfb429ce9a8fdad5b4beb6bb6211e
sgaleraalq <sgaleraalq@gmail.com>
2025-03-20 20:53:15 +0100

    Card image added to project

--- app/src/main/java/com/sgale/card3d/ui/MainActivity.kt
+++ app/src/main/java/com/sgale/card3d/ui/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.sgale.card3d.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,8 +36,10 @@ fun Card3D(
     Box(
         modifier = modifier.fillMaxSize()
     ){
-        Text(
-            "This is just an example"
+        Image(
+            modifier = Modifier.align(Alignment.Center),
+            painter = painterResource(R.drawable.img_example),
+            contentDescription = null,
         )
     }
 }
