b08e41848550d6a5e3d49914e0c2bd05eeeacca1
sgaleraalq <sgaleraalq@gmail.com>
2025-03-20 17:32:55 +0100

    Activity simplified

--- app/src/main/java/com/sgale/card3d/ui/MainActivity.kt
+++ app/src/main/java/com/sgale/card3d/ui/MainActivity.kt
@@ -4,14 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.sgale.card3d.ui.main.Card3D
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Card3D()
+            Scaffold { innerPadding ->
+                Card3D(
+                    modifier = Modifier.padding(innerPadding)
+                )
+            }
         }
+    }
+}
+
+@Composable
+fun Card3D(
+    modifier: Modifier
+) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ){
+        Text(
+            "This is just an example"
+        )
     }
 }
