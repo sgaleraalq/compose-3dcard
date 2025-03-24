81bc7b9b1a380f00c0f794a61470289376c4f99c
sgaleraalq <sgaleraalq@gmail.com>
2025-03-24 17:54:45 +0100

    Shimmer effect on top of card. Missing correct behavior

--- compose-3dcard/src/main/java/com/sgale/compose3d/Compose3D.kt
+++ compose-3dcard/src/main/java/com/sgale/compose3d/Compose3D.kt
@@ -16,7 +16,7 @@
 
 package com.sgale.compose3d
 
-import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
@@ -26,12 +26,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -39,19 +36,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 
@@ -61,7 +60,10 @@ public fun Compose3DCard(
     img: Int,
     shape: RoundedCornerShape = RoundedCornerShape(16.dp)
 ) {
-    val density = LocalDensity.current.density
+    val density = LocalDensity.current
+
+    var offset by remember { mutableStateOf(Offset(0f, 0f)) }
+    var size by remember { mutableStateOf(IntSize.Zero) }
     var rotationX by remember { mutableFloatStateOf(0f) }
     var rotationY by remember { mutableFloatStateOf(0f) }
 
@@ -82,51 +84,63 @@ public fun Compose3DCard(
                 rotationX = rotationX,
                 rotationY = rotationY,
                 transformOrigin = TransformOrigin.Center,
-                cameraDistance = 12f * density
+                cameraDistance = 12f * density.density
             )
             .shadow(
                 elevation = 16.dp,
                 shape = shape
             )
-            .clip(shape),
+            .clip(shape)
+            .onGloballyPositioned { layoutCoordinates ->
+                val positionInRoot = layoutCoordinates.positionInRoot()
+                offset = Offset(positionInRoot.x, positionInRoot.y)
+                size = layoutCoordinates.size
+            },
         painter = painterResource(img),
         contentDescription = null,
         contentScale = ContentScale.Crop
     )
 
     ShimmerEffect(
-        offset = Offset(0f, 0f),
-        size = IntSize(100, 100),
+        modifier = modifier,
+        density = density,
+        size = size,
         rotationX = rotationX,
-        rotationY = rotationY
+        rotationY = rotationY,
+        shape = shape
     )
 }
 
 @Composable
 public fun ShimmerEffect(
-    offset: Offset,
+    modifier: Modifier,
+    density: Density,
     size: IntSize,
     rotationX: Float,
-    rotationY: Float
-){
+    rotationY: Float,
+    shape: RoundedCornerShape
+) {
     Box(
-        modifier = Modifier
-            .offset(offset.x.dp, offset.y.dp)
-            .height(size.height.dp)
-            .size(size.width.dp)
+        modifier = modifier
+            .size(
+                width = with(density) { size.width.toDp() },
+                height = with(density) { size.height.toDp() }
+            )
             .graphicsLayer(
                 rotationX = rotationX,
                 rotationY = rotationY,
-                transformOrigin = TransformOrigin.Center
+                transformOrigin = TransformOrigin.Center,
+                cameraDistance = 12f * density.density
             )
-            .background(Color.Red)
+            .clip(shape)
             .shimmerEffect()
     )
 }
 
-public fun Modifier.shimmerEffect() : Modifier = composed {
-    val transition  = rememberInfiniteTransition()
-    var size        by remember { mutableStateOf(IntSize.Zero) }
+
+public fun Modifier.shimmerEffect(): Modifier = composed {
+    val transition = rememberInfiniteTransition()
+    var size by remember { mutableStateOf(IntSize.Zero) }
 
     val startX by transition.animateFloat(
         initialValue = -2 * size.width.toFloat(),
@@ -134,29 +148,19 @@ public fun Modifier.shimmerEffect() : Modifier = composed {
         animationSpec = infiniteRepeatable(
             animation = tween(
                 durationMillis = 2000,
-                easing = LinearOutSlowInEasing
+                easing = LinearEasing
             ),
             repeatMode = RepeatMode.Restart
         )
     )
 
-
     background(
         brush = Brush.linearGradient(
-            colors = listOf(Color.Gray, Color.LightGray),
+            colors = listOf(LightGray, Transparent, LightGray),
             start = Offset(startX, 0f),
             end = Offset(startX + size.width.toFloat(), size.height.toFloat())
         )
-    )
-    background(
-        brush = Brush.linearGradient(
-            colors = listOf(Color.LightGray, Color.Gray),
-            start = Offset(startX, 0f),
-            end = Offset(startX + size.width.toFloat(), size.height.toFloat())
-        )
-    )
-    onGloballyPositioned {
+    ).onGloballyPositioned {
         size = it.size
     }
-
 }
